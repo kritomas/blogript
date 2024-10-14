ecd6f269a67767c980c6bcfe3f2e7863d215fe9f
kritomas <Unknown>
2024-10-14 08:38:22 +0200

    Finished DB API.

--- api/database.js
+++ api/database.js
@@ -2,5 +2,26 @@ import fs from "fs";
 import mysql from "mysql2";
 
 const pool = mysql.createPool(JSON.parse(fs.readFileSync("sql_credentials.json", "utf8"))).promise();
-const result = await pool.query("select * from Post");
-console.log(result);
+
+export async function getAllPosts()
+{
+	const result = await pool.query("select id, author, content, creation_date from Post;");
+	const rows = result[0];
+	return rows;
+}
+export async function getPost(id)
+{
+	const result = await pool.query("select id, author, content, creation_date from Post where id = ?;", [id]);
+	const rows = result[0];
+	return rows[0];
+}
+export async function createPost(author, content)
+{
+	const result = await pool.query("insert into Post (author, content) values (?, ?);", [author, content]);
+	return await getPost(result[0].insertId);
+}
+export async function removePost(id)
+{
+	const result = await pool.query("delete from Post where id = ?", [id]);
+	return result[0].affectedRows;
+}
