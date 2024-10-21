3ba99a7abf803bc60475788e7ea1e066002c2c3f
kritomas <Unknown>
2024-10-21 08:06:23 +0200

    Added users to db.

--- database/init.sql
+++ database/init.sql
@@ -1,7 +1,17 @@
+create table User
+(
+	id varchar(36) primary key default UUID(),
+	name varchar(32) not null,
+	password_hash varchar(4096) not null,
+	is_admin bit not null default 0
+);
+
 create table Post
 (
 	id integer primary key auto_increment,
+	User_id varchar(36) not null,
 	creation_date date not null default now(),
 	author varchar(32) not null,
-	content varchar(4096)
+	content varchar(4096) not null,
+	foreign key (User_id) references User(id)
 );
