e6a0fb1cc7321a86f88b229c080b9785083a99a8
kritomas <Unknown>
2024-10-21 08:39:59 +0200

    Added users to db api.

--- api/database.js
+++ api/database.js
@@ -1,5 +1,6 @@
 import fs from "fs";
 import mysql from "mysql2";
+import bcrypt from "bcrypt";
 
 const pool = mysql.createPool(JSON.parse(fs.readFileSync("sql_credentials.json", "utf8"))).promise();
 
@@ -31,3 +32,32 @@ export async function updatePost(id, author, content)
 	if (result[0].affectedRows <= 0) return undefined;
 	return await getPost(id);
 }
+
+export async function getUser(username, password)
+{
+	let result = await pool.query("select password_hash from User where username = ?;", [username]);
+	if (result[0].length <= 0) return {};
+	const password_hash = result[0][0]["password_hash"];
+	if (!bcrypt.compare(password, password_hash))
+	{
+		return {};
+	}
+	result = await pool.query("select id from User where username = ?;", [username]);
+	const rows = result[0];
+	return rows[0];
+}
+export async function createUser(username, password)
+{
+	const password_hash = await bcrypt.hash(password, 10);
+	const result = await pool.query("insert into User (username, password_hash) values (?, ?);", [username, password_hash]);
+	return await getUser(username, password);
+}
+export async function removeUser(id)
+{
+	const result = await pool.query("delete from User where id = ?;", [id]);
+	return result[0].affectedRows;
+}
+
+console.log(await removeUser("0dab64f6-8f77-11ef-a4f4-e00af6b185a1"));
+console.log(await createUser("kritomas", "YOOO"));
+console.log(await getUser("kritomas", "YOOO"));
