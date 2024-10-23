145a40cb9237a9b0607f1585a9ff396d278b35ac
kritomas <Unknown>
2024-10-23 07:47:37 +0200

    Added auth to edit page.

--- blogript/edit/edit.js
+++ blogript/edit/edit.js
@@ -1,11 +1,19 @@
-const form = document.getElementById("new-post-form");
+const form = document.getElementById("edit-form");
 
 form.addEventListener("submit", async (event) =>
 {
 	event.preventDefault();
 
 	const formData = new FormData(form);
+
+	if (sessionStorage.userid === undefined)
+	{
+		window.location.replace("/login");
+		return;
+	}
+
 	const data = Object.fromEntries(formData.entries());
+	data.user_id = sessionStorage.userid;
 
 	const urlParams = new URL(window.location.toLocaleString()).searchParams;
 
