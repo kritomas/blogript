9c31a3ada06b2f1cde1cea24f5a7a18cdd6b2ccb
kritomas <Unknown>
2024-10-30 13:20:29 +0100

    Added blacklist table.

--- database/init.sql
+++ database/init.sql
@@ -14,3 +14,12 @@ create table Post
 	content varchar(4096) not null,
 	foreign key (User_id) references User(id)
 );
+
+create table Blacklist
+(
+	id integer primary key auto_increment,
+	User_id varchar(36) not null,
+	Post_id integer not null,
+	foreign key (User_id) references User(id),
+	foreign key (Post_id) references Post(id)
+);
