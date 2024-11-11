7dd21a9707ed9a8fc7c8326b2d6e0c057093948d
kritomas <Unknown>
2024-11-11 08:26:13 +0100

    Added Shark's aggregate (broken).

---
+++ blogript/external/shark/parser.js
@@ -0,0 +1,62 @@
+const url = "http://18.199.158.215:8080";
+
+function parseByJson(post)
+{
+	result = "<div class=\"post\">";
+	result += "Posted at " + post.date + ":</p>";
+	result += "<p>" + post.text + "</p>";
+	result += "</div>"
+	return result;
+}
+async function parseById(id)
+{
+	try
+	{
+		const response = await fetch("/api/notes/" + id);
+		if (!response.ok) throw response;
+		const data = await response.json();
+		result = "<div class=\"post-space\">";
+		result += parseByJson(p);
+		result += "</div>";
+		return result;
+	}
+	catch (error)
+	{
+		window.location.replace("/error");
+		throw error;
+	}
+}
+async function parseAll()
+{
+	if (sessionStorage.userid === undefined)
+	{
+		window.location.replace("/login");
+		return;
+	}
+	try
+	{
+		const response = await fetch(url + "/api/notes/");
+		if (!response.ok) throw response;
+		const data = await response.json();
+		result = "<div class=\"post-space\">";
+		data.forEach((item, i) =>
+		{
+			result += parseByJson(item);
+		});
+		result += "</div>";
+		return result;
+	}
+	catch (error)
+	{
+		window.location.replace("/error");
+		throw error;
+	}
+}
+
+async function insertIntoDOM()
+{
+	html = await parseAll();
+	document.getElementById("post-space").innerHTML = html;
+}
+
+insertIntoDOM();
